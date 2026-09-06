7a5ce27d83021869c3980423318fd3a877386037
ID-Inyang <bobbyinyang9@gmail.com>
2026-09-06 02:30:42 +0100

    Add hello route and update server logging format

--- src/server.js
+++ src/server.js
@@ -3,9 +3,13 @@ const express = require('express')
 
 const app = express();
 
+app.get("/hello", (req, res) => {
+    res.json({message: "Hello World!"})
+});
+
 const PORT = 5002;
 
 const server = app.listen(PORT, ()=> {
-    console.log(chalk.green(`Server running on PORT ${PORT}`));
-    console.log(chalk.bold('The drop'))
+    console.log(chalk.blue(`Server running on PORT ${PORT}`));
+    console.log(chalk.bold.blueBright(`http://localhost:${PORT}`));
 })
