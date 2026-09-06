5249ad6d3742eebaa036236bf51c0af5aba09b78
ID-Inyang <bobbyinyang9@gmail.com>
2026-09-06 19:55:43 +0100

    fix: update PrismaClient logging configuration for development and production environments

--- src/config/db.js
+++ src/config/db.js
@@ -1,5 +1,5 @@
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient({
-    log: process.env.NODE_ENV === 'development',
+    log: process.env.NODE_ENV === 'development' ? ['query', 'info', 'warn', 'error'] : ['error'],
 });
