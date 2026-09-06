c59e6066493b00cf9de32f80ce16292c99da05a1
ID-Inyang <bobbyinyang9@gmail.com>
2026-09-06 20:13:48 +0100

    feat: add database connection and disconnection functions for Prisma

--- src/config/db.js
+++ src/config/db.js
@@ -3,3 +3,24 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient({
     log: process.env.NODE_ENV === 'development' ? ['query', 'info', 'warn', 'error'] : ['error'],
 });
+
+const connectDB = async () => {
+    try {
+        await prisma.$connect();
+        console.log(chalk.green(`DB connected via Prisma`));
+    } catch (error) {
+        console.error(chalk.red(`Database connection failed: ${error.message}`));
+        process.exit(1);
+    }
+}
+
+const disconnectDB = async () => {
+    try {
+        await prisma.$disconnect();
+        console.log(chalk.yellow(`DB disconnected`));
+    } catch (error) {
+        console.error(chalk.red(`Database disconnection failed: ${error.message}`));
+    }
+}
+
+export { prisma, connectDB, disconnectDB };
