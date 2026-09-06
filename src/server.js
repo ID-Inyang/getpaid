0559473caa46bbc5081e9c2f4a7cb6d1292854ca
ID-Inyang <bobbyinyang9@gmail.com>
2026-09-06 21:17:19 +0100

    feat: define User, Movie, and Watchlist models in Prisma schema; add migration for User table

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ import { config } from 'dotenv';
 
 // Load environment variables
 config();
+connectDB();
 
 // Import routes
 import movieRoutes from './routes/movieRoutes.js';
@@ -15,4 +16,33 @@ app.use('/movies', movieRoutes);
 
 app.listen(PORT, () => {
     console.log(chalk.green(`Server is running on http://localhost:${PORT}`));
+});
+
+// Handle unhandled promise rejections (e.g., database connection issues)
+process.on('unhandledRejection', (reason, promise) => {
+    console.error(chalk.red(`Unhandled Rejection at: ${promise}, reason: ${reason.message}`));
+    // Close the database connection
+    server.close( async () => {
+    await disconnectDB();
+    process.exit(1);
+    })
+});
+
+// Handle uncaught exceptions (e.g., programming errors)
+process.on('uncaughtException', (error) => {
+    console.error(chalk.red(`Uncaught Exception: ${error.message}`));
+    // Close the database connection
+    server.close( async () => {
+    await disconnectDB();
+    process.exit(1);
+    })
+});
+
+// Gracefully handle termination signals (e.g., Ctrl+C)
+process.on('SIGINT', async () => {
+    console.log(chalk.yellow('Received SIGINT. Closing server and disconnecting from DB...'));
+    server.close( async () => {
+        await disconnectDB();
+        process.exit(0);
+    })
 });
