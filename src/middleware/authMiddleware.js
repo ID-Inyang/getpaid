1e89e5cf3d32778eb24183b361605de76069e9c4
ID-Inyang <bobbyinyang9@gmail.com>
2026-09-15 03:09:02 +0100

    feat: update authMiddleware to accept request, response, and next parameters

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -4,8 +4,9 @@ import { prisma } from "../config/db.js";
 // Read the token from the request headers
 // Check if the token is valid and decode it
 
-const authMiddleware = async () => {
+const authMiddleware = async (req, res, next) => {
     console.log("Auth Middleware reached");
+    next();
 }
 
 export default authMiddleware;
